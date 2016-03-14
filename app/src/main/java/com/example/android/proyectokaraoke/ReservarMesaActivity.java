2d568c3bb0b9831f6d0bc4970ea0f593e221a465
razegarra <razegarra@gmail.com>
2016-03-14 01:14:15 -0500

    Merge remote-tracking branch 'origin/master'

---
+++ app/src/main/java/com/example/android/proyectokaraoke/ReservarMesaActivity.java
@@ -0,0 +1,23 @@
+package com.example.android.proyectokaraoke;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ReservarMesaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservar_mesa);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+    }
+
+}
