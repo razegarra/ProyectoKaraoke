c10c1e10279ca02afb222c36e41ae0348e5fe7f1
sValH <svaldiviahuaman@gmail.com>
2016-03-19 20:34:37 -0500

    pase de pantallas

--- app/src/main/java/com/example/android/proyectokaraoke/MenuPrincipal.java
+++ app/src/main/java/com/example/android/proyectokaraoke/MenuPrincipal.java
@@ -9,16 +9,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
-public class Bienvenida extends AppCompatActivity {
-    private Button btnIngreso;
+public class MenuPrincipal extends AppCompatActivity {
+private Button btnPiqueos ,btnCanciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bienvenida);
+        setContentView(R.layout.activity_menu_principal);
 
-        btnIngreso = (Button) findViewById(R.id.btnIngreso);
-
+        btnPiqueos = (Button) findViewById(R.id.btnPiqueos);
+        btnCanciones = (Button) findViewById(R.id.btnCanciones);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -31,32 +31,35 @@ public class Bienvenida extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
-
     }
-
 
     @Override
     protected void onResume() {
         super.onResume();
         //Evento click del boton mostrar el cual cargara la información en un toast
-        btnIngreso.setOnClickListener(new View.OnClickListener() {
+        btnPiqueos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
             }
         });
-        btnIngreso.setOnClickListener(new View.OnClickListener() {
+        btnPiqueos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 //Generando el pase de pantalla
-                Intent intent = new Intent(Bienvenida.this, MenuPrincipal.class);
-
+                Intent intent = new Intent(MenuPrincipal.this, PiqueoCatalogoActivity.class);
+                Intent intent1 = new Intent(MenuPrincipal.this,ReservarMesaActivity.class);
                 //Ejecutamos el pase
                 startActivity(intent);
             }
         });
 
     }
+
+
+
+
+
+
 }
