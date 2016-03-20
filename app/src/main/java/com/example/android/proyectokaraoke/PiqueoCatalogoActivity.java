7a12f934b9c79ab5f710202d4102ec38e040b61a
razegarra <razegarra@gmail.com>
2016-03-19 20:32:01 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml
    #	app/src/main/java/com/example/android/proyectokaraoke/AdapterCustom.java

--- app/src/main/java/com/example/android/proyectokaraoke/PiqueoCatalogoActivity.java
+++ app/src/main/java/com/example/android/proyectokaraoke/PiqueoCatalogoActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
 import com.example.android.proyectokaraoke.Entity.CatalogoPiqueo;
+import com.example.android.proyectokaraoke.Util.AdapterRecyclerPiqueoCustom;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -53,11 +54,11 @@ public class PiqueoCatalogoActivity extends AppCompatActivity {
             listaData.add(catalogoPiqueo);
         }
 
-        RecyclerView.Adapter adapter= new AdapterRecyclerCustom(this,listaData);
+        RecyclerView.Adapter adapter= new AdapterRecyclerPiqueoCustom(this,listaData);
         recyclerView.setAdapter(adapter);
 
 
-        );
+
 
 
     }
--- app/src/main/java/com/example/android/proyectokaraoke/Util/AdapterRecyclerPiqueoCustom.java
+++ app/src/main/java/com/example/android/proyectokaraoke/Util/AdapterRecyclerPiqueoCustom.java
@@ -1,10 +1,12 @@
 package com.example.android.proyectokaraoke.Util;
 
 import android.content.Context;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,26 +19,60 @@ import java.util.List;
 /**
  * Created by USUARIO1 on 19/03/2016.
  */
-public class AdapterRecyclerPiqueoCustom extends ArrayAdapter {
+public class AdapterRecyclerPiqueoCustom extends RecyclerView.Adapter<AdapterRecyclerPiqueoCustom.CustomViewHolder> {
+    Context context;  List<CatalogoPiqueo> listaPiqueo;
 
-    public AdapterRecyclerPiqueoCustom(Context context, int resource, List objects) {
-        super(context, resource, objects);
+    public AdapterRecyclerPiqueoCustom(Context context, List<CatalogoPiqueo> listaPiqueo) {
+        this.context = context;
+        this.listaPiqueo=listaPiqueo;
     }
 
     @Override
-    public View getView (int position, View convertView, ViewGroup parent){
-        CatalogoPiqueo catalogoPiqueo = (CatalogoPiqueo) getItem(position);
-        View view = LayoutInflater.from(getContext()).inflate(R.layout.list_row_piqueo,null);
-
-        ImageView imgComida = (ImageView) view.findViewById(R.id.imgcomida);
-        TextView txtTitulo = (TextView) view.findViewById(R.id.title);
-        TextView txtDescripcion = (TextView) view.findViewById(R.id.descripcion);
-        TextView txtPrecio = (TextView) view.findViewById(R.id.precio);
-
-        imgComida.setImageResource(catalogoPiqueo.getImagen());
-        txtTitulo.setText(catalogoPiqueo.getTitulo());
-        txtDescripcion.setText(catalogoPiqueo.getDescripcion());
-        txtPrecio.setText(catalogoPiqueo.getPrecio());
-        return view;
+    public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).
+                inflate(R.layout.list_row_piqueo, null);
+        CustomViewHolder customViewHolder = new CustomViewHolder(view);
+        Log.d("AppRecycler-Create", "create");
+        return customViewHolder;
     }
+
+    @Override
+    public void onBindViewHolder(CustomViewHolder viewHolder, int position) {
+        CatalogoPiqueo catalogoPiqueo = listaPiqueo.get(position);
+        viewHolder.imgImagen.setImageResource(catalogoPiqueo.getImagen());
+        viewHolder.txtTitulo.setText(catalogoPiqueo.getTitulo());
+        viewHolder.txtDescripcion.setText(catalogoPiqueo.getDescripcion());
+        viewHolder.txtPrecio.setText(catalogoPiqueo.getPrecio());
+        Log.d("AppRecycler", "pos " + position);
+    }
+    public class CustomViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private ImageView imgImagen;
+        private TextView txtTitulo;
+        private TextView txtDescripcion;
+        private TextView txtPrecio;
+
+        public CustomViewHolder(View view) {
+            super(view);
+            this.imgImagen = (ImageView) view.findViewById(R.id.imgcomida);
+            this.txtTitulo = (TextView) view.findViewById(R.id.title);
+            this.txtDescripcion = (TextView) view.findViewById(R.id.descripcion);
+            this.txtPrecio = (TextView) view.findViewById(R.id.precio);
+
+            //imgCatalogoPiqueo.setOnClickListener(this);
+            view.setOnClickListener(this);
+        }
+
+
+        @Override
+        public void onClick(View v) {
+            int position = getLayoutPosition();
+            CatalogoPiqueo catalogoPiqueo = listaPiqueo.get(position);
+            Snackbar.make(v, "CatalogoPiqueo Seleccionado: " + catalogoPiqueo.getTitulo(), Snackbar.LENGTH_LONG).show();
+        }
+    }
+    @Override
+    public int getItemCount() {
+        return null==listaPiqueo? 0: listaPiqueo.size();
+    }
+
 }
